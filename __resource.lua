e69802f728bdacf0d00c0860af9098cd7dc6b515
Ollie <38867365+OllieSteam@users.noreply.github.com>
2022-01-20 19:00:37 +0000

    Uploaded
    Uploaded Resource

---
+++ __resource.lua
@@ -0,0 +1,357 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+description "Car Add-on"
+
+files {
+    'data/handling.meta',
+--    'data/vehiclelayouts.meta',
+    'data/vehicles.meta',
+    'data/carcols.meta',
+    'data/carvariations.meta',
+--    'data/contentunlocks.meta',
+    'data/vehiclemodelsets.meta',
+    'data/traffic/popcycle.dat',
+    'data/traffic/popgroups.ymt',
+-- Audio Files
+    'data/audio/argento_game.dat151.rel',
+    'data/audio/argento_sounds.dat54.rel',
+    'data/audio/ariant_game.dat151.rel',
+    'data/audio/ariant_sounds.dat54.rel',
+    'data/audio/b1cjmbuffalo_amp.dat10.rel',
+    'data/audio/b1cjmbuffalo_game.dat151.rel',
+    'data/audio/b1cjmbuffalo_sounds.dat54.rel',
+    'data/audio/bansheepo_game.dat151.rel',
+    'data/audio/bansheepo_sounds.dat54.rel',
+    'data/audio/boxvilleretro_game.dat151.rel',
+    'data/audio/boxvilleretro_sounds.dat54.rel',
+    'data/audio/buffaloac_game.dat151.rel',
+    'data/audio/buffaloac_sounds.dat54.rel',
+    'data/audio/buffaloh_game.dat151.rel',
+    'data/audio/buffaloh_sounds.dat54.rel',
+    'data/audio/castella_game.dat151.rel',
+    'data/audio/castella_sounds.dat54.rel',
+    'data/audio/dlccoq8_game.dat151.rel',
+    'data/audio/dlcdillo_amp.dat10.rel',
+    'data/audio/dlcdillo_game.dat151.rel',
+    'data/audio/dlcdillo_sounds.dat54.rel',
+    'data/audio/dlctorero2_game.dat151.rel',
+    'data/audio/dlctorero2_sounds.dat54.rel',
+    'data/audio/elegyx_game.dat151.rel',
+    'data/audio/elegyx_sounds.dat54.rel',
+    'data/audio/emperor_game.dat151.rel',
+    'data/audio/emperor_sounds.dat54.rel',
+    'data/audio/euros_game.dat151.rel',
+    'data/audio/euros_sounds.dat54.rel',
+    'data/audio/forklift2_game.dat151.rel',
+    'data/audio/forklift2_sounds.dat54.rel',
+    'data/audio/gresleyh_amp.dat10.rel',
+    'data/audio/gresleyh_game.dat151.rel',
+    'data/audio/gresleyh_sounds.dat54.rel',
+    'data/audio/hachura_amp.dat10.rel',
+    'data/audio/hachura_game.dat151.rel',
+    'data/audio/hachura_sounds.dat54.rel',
+    'data/audio/hauler3_amp.dat10.rel',
+    'data/audio/hauler3_game.dat151.rel',
+    'data/audio/hauler3_sounds.dat54.rel',
+    'data/audio/infernussr_amp.dat10.rel',
+    'data/audio/infernussr_game.dat151.rel',
+    'data/audio/infernussr_sounds.dat54.rel',
+    'data/audio/limo_game.dat151.rel',
+    'data/audio/limo_sounds.dat54.rel',
+    'data/audio/majimagt_game.dat151.rel',
+    'data/audio/majimagt_sounds.dat54.rel',
+    'data/audio/meteor_amp.dat10.rel',
+    'data/audio/meteor_game.dat151.rel',
+    'data/audio/meteor_sounds.dat54.rel',
+    'data/audio/mf1c_game.dat151.rel',
+    'data/audio/mf1c_sounds.dat54.rel',
+    'data/audio/mf1_game.dat151.rel',
+    'data/audio/mf1_sounds.dat54.rel',
+    'data/audio/monroec_amp.dat10.rel',
+    'data/audio/monroec_game.dat151.rel',
+    'data/audio/monroec_sounds.dat54.rel',
+    'data/audio/nebulaw_game.dat151.rel',
+    'data/audio/nmule_game.dat151.rel',
+    'data/audio/nmule_sounds.dat54.rel',
+    'data/audio/nspeedo_game.dat151.rel',
+    'data/audio/nspeedo_sounds.dat54.rel',
+    'data/audio/picadorexr_game.dat151.rel',
+    'data/audio/picadorexr_sounds.dat54.rel',
+    'data/audio/romero_game.dat151.rel',
+    'data/audio/romero_sounds.dat54.rel',
+    'data/audio/s230_game.dat151.rel',
+    'data/audio/s230_sounds.dat54.rel',
+    'data/audio/sbus2_amp.dat10.rel',
+    'data/audio/sbus2_game.dat151.rel',
+    'data/audio/sbus2_sounds.dat54.rel',
+    'data/audio/sentgtr_game.dat151.rel',
+    'data/audio/sentgtr_sounds.dat54.rel',
+    'data/audio/sentinelsg4_game.dat151.rel',
+    'data/audio/sentinelsg4_sounds.dat54.rel',
+    'data/audio/sigma3_game.dat151.rel',
+    'data/audio/sigma3_sounds.dat54.rel',
+    'data/audio/spritzer_game.dat151.rel',
+    'data/audio/spritzer_sounds.dat54.rel',
+    'data/audio/stratumc_amp.dat10.rel',
+    'data/audio/stratumc_game.dat151.rel',
+    'data/audio/stratumc_sounds.dat54.rel',
+    'data/audio/sultan2c_game.dat151.rel',
+    'data/audio/sultan2c_sounds.dat54.rel',
+    'data/audio/supergts_game.dat151.rel',
+    'data/audio/supergts_sounds.dat54.rel',
+    'data/audio/tachyon_game.dat151.rel',
+    'data/audio/tachyon_sounds.dat54.rel',
+    'data/audio/tampar_game.dat151.rel',
+    'data/audio/tampar_sounds.dat54.rel',
+    'data/audio/trumpetzrc_game.dat151.rel',
+    'data/audio/trumpetzrc_sounds.dat54.rel',
+    'data/audio/trumpetzr_game.dat151.rel',
+    'data/audio/trumpetzr_sounds.dat54.rel',
+    'data/audio/turismo_game.dat151.rel',
+    'data/audio/turismo_sounds.dat54.rel',
+    'data/audio/urban_game.dat151.rel',
+    'data/audio/urban_sounds.dat54.rel',
+    'data/audio/ventoso_game.dat151.rel',
+    'data/audio/ventoso_sounds.dat54.rel',
+    'data/audio/vesper_game.dat151.rel',
+    'data/audio/vesper_sounds.dat54.rel',
+    'data/audio/vorstand_game.dat151.rel',
+    'data/audio/vorstand_sounds.dat54.rel',
+    'data/audio/zionks_game.dat151.rel',
+    'data/audio/zionks_sounds.dat54.rel',
+-- NPC Audio Files
+    'data/sfx/dlc_argento/argento.awc',
+    'data/sfx/dlc_argento/argento_npc.awc',
+    'data/sfx/dlc_ariant/ariant.awc',
+    'data/sfx/dlc_ariant/ariant_npc.awc',
+    'data/sfx/dlc_bansheepo/bansheepo.awc',
+    'data/sfx/dlc_bansheepo/bansheepo_npc.awc',
+    'data/sfx/dlc_castella/castella.awc',
+    'data/sfx/dlc_castella/castella_npc.awc',
+    'data/sfx/dlc_digital/digital.awc',
+    'data/sfx/dlc_digital/digital_npc.awc',
+    'data/sfx/dlc_dillo/dillo.awc',
+    'data/sfx/dlc_dillo/dillo_npc.awc',
+    'data/sfx/dlc_elegyx/elegyx.awc',
+    'data/sfx/dlc_elegyx/elegyx_npc.awc',
+    'data/sfx/dlc_gresleyh/gresleyh.awc',
+    'data/sfx/dlc_gresleyh/gresleyh_npc.awc',
+    'data/sfx/dlc_gtrsent/gtrsent.awc',
+    'data/sfx/dlc_gtrsent/gtrsent_npc.awc',
+    'data/sfx/dlc_hachura/hachura.awc',
+    'data/sfx/dlc_hachura/hachura_npc.awc',
+    'data/sfx/dlc_hauler3/hauler3.awc',
+    'data/sfx/dlc_hauler3/hauler3_npc.awc',
+    'data/sfx/dlc_infernussr/infernussr.awc',
+    'data/sfx/dlc_infernussr/infernussr_npc.awc',
+    'data/sfx/dlc_jmbuffalo/jmbuffalo.awc',
+    'data/sfx/dlc_jmbuffalo/jmbuffalo_npc.awc',
+    'data/sfx/dlc_limo/limo.awc',
+    'data/sfx/dlc_limo/limo_npc.awc',
+    'data/sfx/dlc_majimagt/majimagt.awc',
+    'data/sfx/dlc_majimagt/majimagt_npc.awc',
+    'data/sfx/dlc_meteor/meteor.awc',
+    'data/sfx/dlc_meteor/meteor_npc.awc',
+    'data/sfx/dlc_monroec/monroec.awc',
+    'data/sfx/dlc_monroec/monroec_npc.awc',
+    'data/sfx/dlc_nmule/nmule.awc',
+    'data/sfx/dlc_nmule/nmule_npc.awc',
+    'data/sfx/dlc_nspeedo/nspeedo.awc',
+    'data/sfx/dlc_nspeedo/nspeedo_npc.awc',
+    'data/sfx/dlc_nw_jester/nw_jester.awc',
+    'data/sfx/dlc_nw_jester/nw_jester_npc.awc',
+    'data/sfx/dlc_picadorexr/picadorexr.awc',
+    'data/sfx/dlc_picadorexr/picadorexr_npc.awc',
+    'data/sfx/dlc_progenmf1/progenmf1.awc',
+    'data/sfx/dlc_progenmf1/progenmf1_npc.awc',
+    'data/sfx/dlc_s230/s230.awc',
+    'data/sfx/dlc_s230/s230_npc.awc',
+    'data/sfx/dlc_sbus2/sbus2.awc',
+    'data/sfx/dlc_sbus2/sbus2_npc.awc',
+    'data/sfx/dlc_sentinelsg4/sentinelsg4.awc',
+    'data/sfx/dlc_sentinelsg4/sentinelsg4_npc.awc',
+    'data/sfx/dlc_sigma3/sigma3.awc',
+    'data/sfx/dlc_sigma3/sigma3_npc.awc',
+    'data/sfx/dlc_spritzer/spritzer.awc',
+    'data/sfx/dlc_spritzer/spritzer_npc.awc',
+    'data/sfx/dlc_sultan2c/sultan2c.awc',
+    'data/sfx/dlc_sultan2c/sultan2c_npc.awc',
+    'data/sfx/dlc_supergts/supergts.awc',
+    'data/sfx/dlc_supergts/supergts_npc.awc',
+    'data/sfx/dlc_tachyon/tachyon.awc',
+    'data/sfx/dlc_tachyon/tachyon_npc.awc',
+    'data/sfx/dlc_tampar/tampar.awc',
+    'data/sfx/dlc_tampar/tampar_npc.awc',
+    'data/sfx/dlc_trumpetzr/trumpetzr.awc',
+    'data/sfx/dlc_trumpetzr/trumpetzr_npc.awc',
+    'data/sfx/dlc_urban/urban.awc',
+    'data/sfx/dlc_urban/urban_npc.awc',
+    'data/sfx/dlc_vesper/vesper.awc',
+    'data/sfx/dlc_vesper/vesper_npc.awc',
+    'data/sfx/dlc_vorstand/vorstand.awc',
+    'data/sfx/dlc_vorstand/vorstand_npc.awc',
+    'data/sfx/dlc_zionks/zionks.awc',
+    'data/sfx/dlc_zionks/zionks_npc.awc',
+    'data/sfx/dlc_zircoflow/zircoflow.awc',
+    'data/sfx/dlc_zircoflow/zircoflow_npc.awc',
+}
+
+data_file 'HANDLING_FILE' 'data/handling.meta'
+--data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+-- data_file 'VEHICLE_SHOP_DLC_FILE' 'data/'
+--data_file 'CONTENT_UNLOCKING_META_FILE' 'data/contentunlocks.meta'
+data_file 'AMBIENT_VEHICLE_MODEL_SET_FILE' 'data/vehiclemodelsets.meta'
+data_file 'POPSCHED_FILE' 'data/traffic/popcycle.dat'
+data_file 'FIVEM_LOVES_YOU_341B23A2F0E0F131' 'data/traffic/popgroups.ymt'
+-- CAR AUDIO
+data_file 'AUDIO_GAMEDATA' 'data/audio/argento_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/argento_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/ariant_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/ariant_sounds.dat'
+data_file 'AUDIO_SYNTHDATA' 'data/audio/b1cjmbuffalo_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/b1cjmbuffalo_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/b1cjmbuffalo_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/bansheepo_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/bansheepo_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/boxvilleretro_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/boxvilleretro_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/buffaloac_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/buffaloac_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/buffaloh_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/buffaloh_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/castella_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/castella_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/dlccoq8_game.dat'
+data_file 'AUDIO_SYNTHDATA' 'data/audio/dlcdillo_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/dlcdillo_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/dlcdillo_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/dlctorero2_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/dlctorero2_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/elegyx_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/elegyx_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/emperor_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/emperor_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/euros_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/euros_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/forklift2_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/forklift2_sounds.dat'
+data_file 'AUDIO_SYNTHDATA' 'data/audio/gresleyh_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/gresleyh_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/gresleyh_sounds.dat'
+data_file 'AUDIO_SYNTHDATA' 'data/audio/hachura_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/hachura_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/hachura_sounds.dat'
+data_file 'AUDIO_SYNTHDATA' 'data/audio/hauler3_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/hauler3_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/hauler3_sounds.dat'
+data_file 'AUDIO_SYNTHDATA' 'data/audio/infernussr_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/infernussr_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/infernussr_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/limo_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/limo_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/majimagt_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/majimagt_sounds.dat'
+data_file 'AUDIO_SYNTHDATA' 'data/audio/meteor_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/meteor_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/meteor_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/mf1c_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/mf1c_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/mf1_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/mf1_sounds.dat'
+data_file 'AUDIO_SYNTHDATA' 'data/audio/monroec_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/monroec_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/monroec_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/nebulaw_game.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/nmule_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/nmule_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/nspeedo_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/nspeedo_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/picadorexr_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/picadorexr_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/romero_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/romero_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/s230_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/s230_sounds.dat'
+data_file 'AUDIO_SYNTHDATA' 'data/audio/sbus2_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/sbus2_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/sbus2_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/sentgtr_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/sentgtr_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/sentinelsg4_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/sentinelsg4_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/sigma3_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/sigma3_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/spritzer_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/spritzer_sounds.dat'
+data_file 'AUDIO_SYNTHDATA' 'data/audio/stratumc_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/stratumc_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/stratumc_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/sultan2c_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/sultan2c_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/supergts_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/supergts_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/tachyon_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/tachyon_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/tampar_game.dat'
+data_file 'AUDIO_SYNTHDATA' 'data/audio/tampar_game'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/tampar_sounds.dat'
+data_file 'AUDIO_SYNTHDATA' 'data/audio/tampar_sounds'
+data_file 'AUDIO_GAMEDATA' 'data/audio/trumpetzrc_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/trumpetzrc_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/trumpetzr_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/trumpetzr_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/turismo_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/turismo_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/urban_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/urban_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/ventoso_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/ventoso_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/vesper_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/vesper_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/vorstand_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/vorstand_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/audio/zionks_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/audio/zionks_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_argento'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_ariant'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_bansheepo'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_castella'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_digital'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_dillo'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_elegyx'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_gresleyh'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_gtrsent'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_hachura'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_hauler3'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_infernussr'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_jmbuffalo'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_limo'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_majimagt'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_meteor'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_monroec'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_nmule'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_nspeedo'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_nw_jester'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_picadorexr'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_progenmf1'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_s230'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_sbus2'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_sentinelsg4'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_sigma3'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_spritzer'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_sultan2c'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_supergts'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_tachyon'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_tampar'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_trumpetzr'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_urban'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_vesper'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_vorstand'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_zionks'
+data_file 'AUDIO_WAVEPACK' 'data/sfx/dlc_zircoflow'
+--client_script {
+--    'data/vehicle_names.lua' 
+--}
